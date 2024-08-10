c5563b0b4228bd895bde1871c132ec3de85a4829
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-08-10 14:37:59 +0700

    Update navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import { IconMenu2 } from "@tabler/icons-react";
+import { IconBell, IconMenu2, IconUserCircle } from "@tabler/icons-react";
 
 export default function Navbar() {
   return (
 
     <nav className="bg-cust-bg-gray w-full h-16 sticky top-0 left-0 flex justify-between items-center px-10 z-50">
       <IconMenu2 stroke={1.5} className=" fill-cust-black w-[24px]" />
-      <div>
-        <p className="text-sm">Asep</p>
-        <small className="text-cust-gray-darker text-xs">Owner</small>
+      <div className="flex items-center gap-8">
+        <IconBell size={24} className="" />
+        <div>
+          <p className="text-sm leading-none">Asep</p>
+          <small className="text-cust-gray-darker text-xs">Owner</small>
+        </div>
+        <IconUserCircle size={28} className="" />
       </div>
     </nav>
 
